f5ba6a30a244bd6d75d9ea85107b9d0c614e656e
irajbathla-alt <irajbathla@gmail.com>
2026-04-27 23:48:07 -0700

    Rename public secret to app key

--- shared-config.js
+++ shared-config.js
@@ -1,2 +1,2 @@
 window.MFJ_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzFaX1x1qPqDqGj1OWpLoJR5mhXx1LA7qARA8ipEQEib-1MJ1DRWyVgi7jvatpJ5rw/exec";
-window.MFJ_SECRET = "JFC2026PRIVATEKEY";
+window.MFJ_APP_KEY = "MFJ_PUBLIC_APP";
