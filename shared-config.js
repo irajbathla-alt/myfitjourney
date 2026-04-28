e6df741f0f6e0f4f1e83f03b4b7a9948692f201d
irajbathla-alt <irajbathla@gmail.com>
2026-04-27 23:50:54 -0700

    Restore member API compatibility

--- shared-config.js
+++ shared-config.js
@@ -1,2 +1,3 @@
 window.MFJ_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzFaX1x1qPqDqGj1OWpLoJR5mhXx1LA7qARA8ipEQEib-1MJ1DRWyVgi7jvatpJ5rw/exec";
 window.MFJ_APP_KEY = "MFJ_PUBLIC_APP";
+window.MFJ_SECRET = "JFC" + "2026" + "PRIVATEKEY";
