2fa35d6556750fb24b1642374bb28a1e3bb68597
irajbathla-alt <irajbathla@gmail.com>
2026-05-09 16:32:57 -0700

    Fix cloud progress sync scripts on bikini and strength pages

--- shared-config.js
+++ shared-config.js
@@ -1,4 +1,4 @@
-window.MFJ_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzFaX1x1qPqDqGj1OWpLoJR5mhXx1LA7qARA8ipEQEib-1MJ1DRWyVgi7jvatpJ5rw/exec";
+window.MFJ_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby6XZ5ThRxOf1SXhLuioSzp_LGWoyR3jyiSp4XSoeYzWgp-sGCJLBSlPSIXA-UDRz8/exec";
 window.MFJ_APP_KEY = "MFJ_PUBLIC_APP";
 window.MFJ_SECRET = "JFC" + "2026" + "PRIVATEKEY";
 window.MFJ_PROGRAMS = [
